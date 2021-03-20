8cad38334baafb6145f65a8e8eba5988e9a347aa
Vinit Kumar <63047288+vinitdeveloper@users.noreply.github.com>
2021-03-20 12:53:31 +0530

    same again uploaded

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// require('dotenv').config()
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
@@ -6,28 +6,28 @@ const app = express()
 app.use(express.json())
 
 
-// app.use((req, res, next) => {
-//     if (req.headers['api-key'] !== "" && req.headers['api-key'] === process.env.API_KEY) {
-//         if (req.headers['content-type'] !== 'application/json' && req.method === 'POST') {
-//             res.status(405).send({ message: 'Method Not Allowed' })
-//         } else {
-//             next()
-//         }
-//     } else {
-//         res.status(400).send({ message: 'invalid Api key' })
-//     }
-// })
+app.use((req, res, next) => {
+    if (req.headers['api-key'] !== "" && req.headers['api-key'] === process.env.API_KEY) {
+        if (req.headers['content-type'] !== 'application/json' && req.method === 'POST') {
+            res.status(405).send({ message: 'Method Not Allowed' })
+        } else {
+            next()
+        }
+    } else {
+        res.status(400).send({ message: 'invalid Api key' })
+    }
+})
 
 app.get('/', (req, res) => {
     res.send('Rest apis working ')
 })
 
 
-// const documentRouter = require('./routes/documents')
-// const hitRouter = require('./routes/hits')
+const documentRouter = require('./routes/documents')
+const hitRouter = require('./routes/hits')
 
-// app.use('/document', documentRouter );
-// app.use('/hits', hitRouter );
+app.use('/document', documentRouter );
+app.use('/hits', hitRouter );
 
 
 // server start
